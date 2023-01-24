0935662243ce76d6fab22c73dd1e1e6bcf03b659
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2023-01-24 14:43:42 +0100

    pt8 done

--- routes/index.js
+++ routes/index.js
@@ -29,11 +29,33 @@ router.get('/', async function (req, res) {
 });
 */
 
+
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
     const [rows] = await promisePool.query("INSERT INTO hl21forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
+    //alert("Det skickades (nog)");
+    res.redirect('/new');
 });
+
+/* // för att man ska kunna skapa användare i formuläret, behövs ändring i html (och annat?)
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+
+    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
+    let user = await promisePool.query('SELECT * FROM hl21user WHERE name = ?', [author]);
+    if (!user) {    // User eller forum ???
+        user = await promisePool.query('INSERT INTO hl21user (name) VALUES (?)', [author]);
+    }
+
+    // user.insertId bör innehålla det nya ID:t för författaren
+
+    const userId = user.insertId || user[0].id;
+
+    // kör frågan för att skapa ett nytt inlägg
+    const [rows] = await promisePool.query('INSERT INTO hl21forum (author, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    //res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
+});
+*/
 
 router.get('/new', async function (req, res, next) {
     const [users] = await promisePool.query("SELECT * FROM hl21users");
